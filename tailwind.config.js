451bf189264b29897a21dfbfff8fcd86bfe5987a
Cristian Andres Fernandez Bermudez <crafernandezbe@unal.edu.co>
2021-07-06 07:44:59 -0500

    first day of todo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: { safelist: [/data-theme$/] },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
